91ab872453aceb9cacdf61094d65ca3ebb25be97
Cedrok <cvautrai@student.42.fr>
2020-08-17 16:04:01 +0200

    Add checkbox for shadows; fix CLI scrollbar width

---
+++ Assets/Scripts/LightControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightControl : MonoBehaviour
+{
+    public void ToggleShadows(bool _value)
+    {
+        if (_value)
+            GetComponent<Light>().shadows = LightShadows.Soft;
+        else
+            GetComponent<Light>().shadows = LightShadows.None;
+    }
+}
