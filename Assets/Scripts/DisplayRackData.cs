6840b9402f182d7e3069775326b48f215d85105a
Cedrok <cvautrai@student.42.fr>
2020-08-18 15:17:41 +0200

    Add a front label for racks

--- Assets/Scripts/DisplayRackData.cs
+++ Assets/Scripts/DisplayRackData.cs
@@ -6,20 +6,26 @@ using UnityEngine;
 [RequireComponent(typeof(Rack))]
 public class DisplayRackData : MonoBehaviour
 {
-    // textName is the root of all texts
-    [SerializeField] private  TextMeshPro textName = null;
+    // textNameRear is the root of all texts
+    [SerializeField] private  TextMeshPro textNameRear = null;
+    [SerializeField] private  TextMeshPro textNameFront = null;
+    [SerializeField] private  TextMeshPro textNameTop = null;
     [SerializeField] private TextMeshPro textDesc = null;
     
     ///<summary>
-    /// Move displayed texts in the top/front of the rack. Also set width of name and description regarding rack width.
+    /// Move displayed texts in the top/front of the rack.
+    /// Also set width of name and description regarding rack width.
     ///</summary>
     public void PlaceTexts()
     {
         Vector3 boxSize = transform.GetChild(0).localScale;
-        textName.transform.localPosition = new Vector3(0, boxSize.y, -boxSize.z) / 2;
-        textName.transform.localPosition += new Vector3(0, -textName.rectTransform.sizeDelta.y / 2, -0.01f);
+        textNameRear.transform.localPosition = new Vector3(0, boxSize.y, -boxSize.z) / 2;
+        textNameRear.transform.localPosition += new Vector3(0, -textNameRear.rectTransform.sizeDelta.y / 2, -0.001f);
+        textNameFront.transform.localPosition = new Vector3(0, 0, boxSize.z + 0.002f);
 
-        textName.rectTransform.sizeDelta = new Vector2(boxSize.x - 0.1f, textName.rectTransform.sizeDelta.y);
+        textNameRear.rectTransform.sizeDelta = new Vector2(boxSize.x - 0.1f, textNameRear.rectTransform.sizeDelta.y);
+        textNameFront.rectTransform.sizeDelta = new Vector2(boxSize.x - 0.1f, textNameRear.rectTransform.sizeDelta.y);
+        textNameTop.rectTransform.sizeDelta = new Vector2(boxSize.x - 0.1f, textNameRear.rectTransform.sizeDelta.y);
         textDesc.rectTransform.sizeDelta = new Vector2(boxSize.x - 0.1f, textDesc.rectTransform.sizeDelta.y);
     }
 
@@ -28,7 +34,9 @@ public class DisplayRackData : MonoBehaviour
     ///</summary>
     public void FillTexts()
     {
-        textName.text = GetComponent<Rack>().name;
+        textNameRear.text = GetComponent<Rack>().name + " (R)";
+        textNameFront.text = GetComponent<Rack>().name + " (F)";
+        textNameTop.text = GetComponent<Rack>().name;
         textDesc.text = GetComponent<Rack>().description;
     }
 }
