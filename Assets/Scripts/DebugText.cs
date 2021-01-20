2aed28d5a8ffcec932b3bf0d0d9e0088a6581cc4
Cedrok <cvautrai@student.42.fr>
2021-01-20 11:31:31 +0100

    Create a scene Perfs with spawner

--- Assets/Scripts/DebugText.cs
+++ Assets/Scripts/DebugText.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using TMPro;
 using UnityEditor;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class DebugText : MonoBehaviour
 {
     private TextMeshProUGUI txt;
+    private int[] last100FPS = new int[100];
+    private int currentIndex = 0;
+    public int averageFPS {get; private set;}
+    public int count {get; private set;}
 
     private void Start()
     {
@@ -16,9 +21,35 @@ public class DebugText : MonoBehaviour
     private void Update()
     {
         int currentFPS = Mathf.RoundToInt(1f / Time.unscaledDeltaTime);
-        int count = GameObject.FindObjectsOfType<GameObject>().Length;
+        if (currentIndex < last100FPS.Length)
+        {
+            last100FPS[currentIndex] = currentFPS;
+            currentIndex++;
+        }
+        else
+        {
+            currentIndex = 0;
+            last100FPS[currentIndex] = currentFPS;
+        }
+        averageFPS = Average(last100FPS);
 
-        txt.text = $"Object count: {count}\nFPS: {currentFPS}";
+        count = GameObject.FindObjectsOfType<GameObject>().Length;
         
+        txt.text = $"Object count: {count}\nFPS: {averageFPS}";
+    }
+
+    ///<summary>
+    /// Compute the average value of an array.
+    ///</summary>
+    ///<param name="_array">The array to compute</param>
+    ///<returns>The average og the array</returns>
+    private int Average(int[] _array)
+    {
+        int sum = 0;
+        for(int i = 0; i < _array.Length; i++)
+        {
+            sum += _array[i];
+        }
+        return sum / _array.Length;
     }
 }
