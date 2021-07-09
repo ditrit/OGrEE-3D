e431bb8b4a72b114f01f4ac75fcddca4a4fe9ce2
Cedrok <cvautrai@student.42.fr>
2021-07-09 11:00:20 +0200

    More precise object counter

--- Assets/Scripts/DebugText.cs
+++ Assets/Scripts/DebugText.cs
@@ -10,9 +10,9 @@ public class DebugText : MonoBehaviour
     private TextMeshProUGUI txt;
     private int[] last100FPS = new int[100];
     private int currentIndex = 0;
-    public int averageFPS {get; private set;}
-    public int goCount {get; private set;}
-    public int ooCount {get; private set;}
+    public int averageFPS { get; private set; }
+    public int goCount { get; private set; }
+    public int ooCount { get; private set; }
 
     private void Start()
     {
@@ -34,17 +34,56 @@ public class DebugText : MonoBehaviour
         }
         averageFPS = Average(last100FPS);
 
-        
         txt.text = $"Objs: {ooCount}/{goCount}\nFPS: {averageFPS}";
     }
 
     ///<summary>
     /// Count the number of OgreeObject and GameObject in the scene.
+    /// Display count of each OgreeObject type in CLI.
     ///</summary>
     public void CountObjects()
     {
+        int tenantsCount = 0;
+        int sitesCount = 0;
+        int buildingsCount = 0;
+        int roomsCount = 0;
+        int racksCount = 0;
+        int devicesCount = 0;
+
         ooCount = GameManager.gm.allItems.Count;
         goCount = GameObject.FindObjectsOfType<GameObject>().Length;
+
+        foreach (DictionaryEntry de in GameManager.gm.allItems)
+        {
+            OgreeObject obj = ((GameObject)de.Value).GetComponent<OgreeObject>();
+            switch (obj.category)
+            {
+                case "tenant":
+                    tenantsCount++;
+                    break;
+                case "site":
+                    sitesCount++;
+                    break;
+                case "building":
+                    buildingsCount++;
+                    break;
+                case "room":
+                    roomsCount++;
+                    break;
+                case "rack":
+                    racksCount++;
+                    break;
+                case "device":
+                    devicesCount++;
+                    break;
+            }
+        }
+        GameManager.gm.AppendLogLine($"Tenants: {tenantsCount}");
+        GameManager.gm.AppendLogLine($"Sites: {sitesCount}");
+        GameManager.gm.AppendLogLine($"Buildings: {buildingsCount}");
+        GameManager.gm.AppendLogLine($"Rooms: {roomsCount}");
+        GameManager.gm.AppendLogLine($"Racks: {racksCount}");
+        GameManager.gm.AppendLogLine($"Devices: {devicesCount}");
     }
 
     ///<summary>
@@ -55,10 +94,9 @@ public class DebugText : MonoBehaviour
     private int Average(int[] _array)
     {
         int sum = 0;
-        for(int i = 0; i < _array.Length; i++)
-        {
+        for (int i = 0; i < _array.Length; i++)
             sum += _array[i];
-        }
+        
         return sum / _array.Length;
     }
 }
