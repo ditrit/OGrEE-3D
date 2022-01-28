d296fe8f682dbe7b704c33dc974cb64fc57981e9
Cedrok <cvautrai@student.42.fr>
2022-01-28 18:05:09 +0100

    Create CliParser for handling received messages from tcp; Update debug api_url; v2.7

---
+++ Assets/Scripts/Network/CliParser.cs
@@ -0,0 +1,201 @@
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CliParser// : MonoBehaviour
+{
+    #region Structures
+    ///<summary>
+    /// Standard structure, helps to know which kind of data in received.
+    ///</summary>
+    struct SData
+    {
+        public string type;
+        public string data;
+    }
+
+    struct SInteract
+    {
+        public string id;
+        public string param;
+        public string value;
+    }
+
+    struct SUiManip
+    {
+        public string command;
+        public string data;
+    }
+
+    struct SCameraManip
+    {
+        public string command;
+        public Vector3 position;
+        public Vector2 rotation;
+    }
+
+    #endregion
+
+    ReadFromJson rfJson = new ReadFromJson();
+
+    ///<summary>
+    /// Deserialize CLI input and parse it. 
+    ///</summary>
+    ///<param name="_input">The json to deserialize</param>
+    public void DeserializeInput(string _input)
+    {
+        GameObject obj = null;
+        SData command = JsonConvert.DeserializeObject<SData>(_input);
+        switch (command.type)
+        {
+            case "load template":
+                rfJson.CreateObjTemplateJson(command.data);
+                break;
+            case "select":
+                obj = Utils.GetObjectById(command.data);
+                if (obj)
+                    GameManager.gm.SetCurrentItem(obj);
+                else
+                    GameManager.gm.AppendLogLine("Error on select", "red");
+                break;
+            case "delete":
+                obj = Utils.GetObjectById(command.data);
+                if (obj)
+                    GameManager.gm.DeleteItem(obj, false); // deleteServer == true ??
+                else
+                    GameManager.gm.AppendLogLine("Error on delete", "red");
+                break;
+            case "focus":
+                obj = Utils.GetObjectById(command.data);
+                if (obj)
+                    GameManager.gm.FocusItem(obj);
+                else
+                    GameManager.gm.AppendLogLine("Error on focus", "red");
+                break;
+            case "create":
+                CreateObjectFromData(_input);
+                break;
+            case "modify":
+                // to do
+                break;
+            case "interact":
+                // to do
+                break;
+            case "ui":
+                ManipulateUi(_input);
+                break;
+            case "camera":
+                ManipulateCamera(_input);
+                break;
+            default:
+                GameManager.gm.AppendLogLine("Unknown type", "red");
+                break;
+        }
+    }
+
+    ///<summary>
+    /// Deserialize given SApiObject and call the good generator.
+    ///</summary>
+    ///<param name="_input">The SApiObject to deserialize</param>
+    private void CreateObjectFromData(string _input)
+    {
+        SApiObject obj = JsonConvert.DeserializeObject<SApiObject>(_input);
+        switch (obj.category)
+        {
+            case "tenant":
+                CustomerGenerator.instance.CreateTenant(obj);
+                break;
+            case "site":
+                CustomerGenerator.instance.CreateSite(obj);
+                break;
+            case "building":
+                BuildingGenerator.instance.CreateBuilding(obj);
+                break;
+            case "room":
+                BuildingGenerator.instance.CreateRoom(obj);
+                break;
+            case "rack":
+                if (obj.attributes["template"] == "")
+                    ObjectGenerator.instance.CreateRack(obj);
+                else
+                    ObjectGenerator.instance.CreateRack(obj, null, false);
+                break;
+            case "device":
+                if (obj.attributes["template"] == "")
+                    ObjectGenerator.instance.CreateDevice(obj);
+                else
+                    ObjectGenerator.instance.CreateDevice(obj, null, false);
+                break;
+            case "corridor":
+                ObjectGenerator.instance.CreateCorridor(obj);
+                break;
+            case "group":
+                ObjectGenerator.instance.CreateGroup(obj);
+                break;
+        }
+    }
+
+    ///<summary>
+    /// Parse a UI command and execute it.
+    ///</summary>
+    ///<param name="_input">The SUiManip to deserialize</param>
+    private void ManipulateUi(string _input)
+    {
+        SUiManip manip = JsonConvert.DeserializeObject<SUiManip>(_input);
+        switch (manip.command)
+        {
+            case "delay":
+                // ?? Still needed ??
+                break;
+            case "infos":
+                if (manip.data == "true")
+                    GameManager.gm.MovePanel("infos", true);
+                else
+                    GameManager.gm.MovePanel("infos", false);
+                break;
+            case "debug":
+                if (manip.data == "true")
+                    GameManager.gm.MovePanel("debug", true);
+                else
+                    GameManager.gm.MovePanel("debug", false);
+                break;
+            case "highlight":
+                GameObject obj = Utils.GetObjectById(manip.data);
+                if (obj)
+                    EventManager.Instance.Raise(new HighlightEvent { obj = obj });
+                else
+                    GameManager.gm.AppendLogLine("Error on highlight", "red");
+                break;
+            default:
+                GameManager.gm.AppendLogLine("Unknown command", "red");
+                break;
+        }
+    }
+
+    ///<summary>
+    /// Parse a camera command and execute it.
+    ///</summary>
+    ///<param name="_input">The SCameraManip to deserialize</param>
+    private void ManipulateCamera(string _input)
+    {
+        SCameraManip manip = JsonConvert.DeserializeObject<SCameraManip>(_input);
+        CameraControl cc = GameObject.FindObjectOfType<CameraControl>();
+        switch (manip.command)
+        {
+            case "move":
+                cc.MoveCamera(manip.position, manip.rotation);
+                break;
+            case "translate":
+                cc.TranslateCamera(manip.position, manip.rotation);
+                break;
+            case "wait":
+                cc.WaitCamera(manip.rotation.y);
+                break;
+            default:
+                GameManager.gm.AppendLogLine("Unknown command", "red");
+                break;
+        }
+
+    }
+}
