603b6a2d1425f49384e0d84ddf546f18743a09e3
Timothé Rios <trios.timothe@hotmail.fr>
2023-03-14 15:15:43 +0100

    Update States.cs

--- Assets/Scripts/Enums_Strucs/States.cs
+++ Assets/Scripts/Enums_Strucs/States.cs
@@ -8,7 +8,16 @@ public class States : MonoBehaviour
 {
     private void Start()
     {
-        print(DeviceEdit.Add(TempColor).Remove(EditOnly));
+        print(TempColor.Add(RackEdit).Add(RackFocus));
+        print(RackFocus.Add(DeviceSelect));
+        foreach(State state1 in states)
+        {
+            foreach(State state2 in states)
+            {
+                print($"{state1.name} + {state2.name} = {state1.Add(state2)}");
+                print($"{state2.name} + {state1.name} = {state2.Add(state1)}");
+            }
+        }
     }
 
     public static Hashtable test = new Hashtable();
@@ -22,7 +31,7 @@ public class States : MonoBehaviour
 
         public abstract bool Contains(State _state);
 
-        public abstract State Remove(State _state);
+        public abstract State Remove(StandAloneState _state);
 
         public abstract State Add(State _state);
 
@@ -63,34 +72,30 @@ public class States : MonoBehaviour
         {
             string Aux(StandAloneState s, int i)
             {
-                if (parent != null)
-                    return $"{new string(' ', i * 2)}{name}\n    <={(s.parent != null ? Aux(s.parent, i + 1) : "")}";
-                return $"{new string(' ', i * 2)}{s.name}";
+                if (s.parent != null)
+                    return $"{s.name}\n{new string(' ', i * 4)}<={Aux(s.parent, i + 1)}";
+                return s.name;
             }
-            return Aux(this, 0);
+            return Aux(this, 1);
         }
 
         public override State Add(State _state)
         {
             if (_state.Contains(this))
-                return _state;
-            if (Contains(_state))
                 return this;
+            if (Contains(_state))
+                return _state;
+            if (_state is StandAloneState SAstate && SAstate.Ancester() == Ancester())
+                return _state;
             if (test.ContainsKey(id | _state.id))
                 return (State)test[id | _state.id];
-            if (_state is CompoundState cState)
-            {
-                List<State> states = cState.impliedStates.GetRange(0, cState.impliedStates.Count);
-                states.Add(this);
-                return new CompoundState(states, $"{name} | {_state.name}");
-            }
             return new CompoundState(new List<State> { this, _state }, $"{name} | {_state.name}");
         }
 
-        public override State Remove(State _state)
+        public override State Remove(StandAloneState _state)
         {
             if (_state.Contains(this))
-                return None;
+                return Idle;
             return this;
         }
 
@@ -113,10 +118,19 @@ public class States : MonoBehaviour
 
         public CompoundState(List<State> _impliedStates, [CallerMemberName] string _name = null)
         {
-            impliedStates = _impliedStates;
+            impliedStates = new List<State>();
+            foreach (State state in _impliedStates)
+            {
+                if (state is StandAloneState)
+                    impliedStates.Add(state);
+                else if (state is CompoundState cState)
+                    foreach (State childState in cState.impliedStates)
+                        impliedStates.Add(childState);
+            }
             name = _name;
             id = 0;
             impliedStates.ForEach(s => id |= s.id);
+            test.Add(id, this);
         }
 
         public override State Ancester()
@@ -126,7 +140,7 @@ public class States : MonoBehaviour
 
         public override bool Contains(State _state)
         {
-            if (_state == this)
+            if (id != 0 && _state.id !=0 && (id & _state.id) == _state.id)
                 return true;
             foreach (State state in impliedStates)
                 if (state.Contains(_state))
@@ -142,26 +156,39 @@ public class States : MonoBehaviour
                 return this;
             if (test.ContainsKey(id | _state.id))
                 return (State)test[id | _state.id];
-            List<State> states = impliedStates.GetRange(0, impliedStates.Count);
+            List<State> states = new List<State>();
+            if (_state is StandAloneState SAState)
+            {
+                foreach (State state in impliedStates)
+                {
+                    if (state is StandAloneState aloneState && aloneState.Ancester() == SAState.Ancester())
+                        continue;
+                    states.Add(state);
+                }
+            }
+            else
+                states = impliedStates.GetRange(0, impliedStates.Count);
             states.Add(_state);
+            int newId = 0;
+            states.ForEach(s => newId |= s.id);
+            if (test.ContainsKey(newId))
+                return (State)test[newId];
             CompoundState newState = new CompoundState(states, $"{name} | {_state.name}");
             return newState;
         }
 
-        public override State Remove(State _state)
+        public override State Remove(StandAloneState _state)
         {
-            if (_state == this)
-                return None;
             List<State> states = impliedStates.GetRange(0, impliedStates.Count);
             states = states.Select(s => s.Remove(_state)).ToList();
-            states.RemoveAll(s => s == None);
+            states.RemoveAll(s => s == Idle);
             int id = 0;
             states.ForEach(s => id |= s.id);
             if (test.ContainsKey(id))
                 return (State)test[id];
             string name = "";
             states.ForEach(s => name += $" | {s.name}");
-            return new CompoundState(states,name.Trim(new char[] { '|',' '}));
+            return new CompoundState(states, name.Trim(new char[] { '|', ' ' }));
         }
         public override string ToString()
         {
@@ -184,7 +211,7 @@ public class States : MonoBehaviour
         }
     }
 
-    public static StandAloneState None = new StandAloneState(0);
+    public static StandAloneState Idle = new StandAloneState(0);
     public static StandAloneState Select = new StandAloneState(1);
     public static StandAloneState FocusOnly = new StandAloneState(2);
     public static StandAloneState EditOnly = new StandAloneState(4);
@@ -197,9 +224,33 @@ public class States : MonoBehaviour
     public static StandAloneState TenantSelect = new StandAloneState(512, Select);
     public static CompoundState DeviceFocus = new CompoundState(new List<State> { FocusOnly, DeviceSelect });
     public static CompoundState DeviceEdit = new CompoundState(new List<State> { EditOnly, DeviceFocus });
-    public static CompoundState RackFocus = new CompoundState(new List<State> { FocusOnly, RackSelect });
+    public static CompoundState RackFocus = new CompoundState(new List<State> { FocusOnly, OObjectSelect });
     public static CompoundState RackEdit = new CompoundState(new List<State> { EditOnly, RackFocus });
     public static StandAloneState TempColor = new StandAloneState(1024);
     public static StandAloneState TempDiagram = new StandAloneState(2048);
     public static StandAloneState ScatterPlot = new StandAloneState(4096);
+    public static StandAloneState HeatMap = new StandAloneState(8192);
+
+    public static List<State> states = new List<State>() 
+    {
+        Idle,
+        Select,
+        FocusOnly,
+        EditOnly,
+        OObjectSelect,
+        DeviceSelect,
+        RackSelect,
+        RoomSelect,
+        BuildingSelect,
+        SiteSelect,
+        TenantSelect,
+        DeviceFocus,
+        DeviceEdit,
+        RackFocus,
+        RackEdit,
+        TempColor,
+        TempDiagram,
+        ScatterPlot,
+        HeatMap
+    };
 }
