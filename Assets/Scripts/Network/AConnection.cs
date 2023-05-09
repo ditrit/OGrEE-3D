1517d14957ef437d1cdb75459ba76e3082a1d8ec
Timothé Rios <trios.timothe@hotmail.fr>
2023-05-09 15:06:19 +0200

    Formatting

--- Assets/Scripts/Network/AConnection.cs
+++ Assets/Scripts/Network/AConnection.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 public abstract class AConnection
 {
     public readonly Queue<string> incomingQueue = new Queue<string>();
-
     public readonly Queue<Action> mainThreadQueue = new Queue<Action>();
     public abstract void StartConnection(int _cliPort);
     public abstract string[] GetMessages();
