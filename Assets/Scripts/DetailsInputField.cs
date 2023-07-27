741a85af1fbcf6bcc9aa1dbc7b98bab5f3aff490
Timothé Rios <trios.timothe@hotmail.fr>
2023-07-27 17:08:20 +0200

    Bug fix
    lock button starts disabled

--- Assets/Scripts/DetailsInputField.cs
+++ Assets/Scripts/DetailsInputField.cs
@@ -51,7 +51,7 @@ public class DetailsInputField : MonoBehaviour
         {
             ActiveInputField(false);
             UpdateInputField("0");
-            lockLodButton.enabled = false;
+            lockLodButton.gameObject.SetActive(false);
         }
         else
         {
