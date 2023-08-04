3d716b3ab2d0bec2e810d4520ce1800b1575fe93
Cédric Vautrain <35805113+Cedrok@users.noreply.github.com>
2023-08-04 12:28:29 +0200

    Tille offset for templated rooms (#377)
    * Apply tileOffset from roomTemplate in Room.SetAreas(); typos
    
    * Correct handling of areas with offset
    
    * Arrays as Lists

--- Assets/Scripts/Enums_Strucs/SMargin.cs
+++ Assets/Scripts/Enums_Strucs/SMargin.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
-using UnityEngine;
 
 [System.Serializable]
 public struct SMargin
@@ -17,39 +15,5 @@ public struct SMargin
         bottom = _data.bottom;
         right = _data.right;
         left = _data.left;
-    }
-
-    public SMargin(float _N, float _S, float _E, float _W)
-    {
-        top = _N;
-        bottom = _S;
-        right = _E;
-        left = _W;
-    }
-
-    public SMargin(string _N, string _S, string _E, string _W)
-    {
-        top = Utils.ParseDecFrac(_N);
-        bottom = Utils.ParseDecFrac(_S);
-        right = Utils.ParseDecFrac(_E);
-        left = Utils.ParseDecFrac(_W);
-    }
-
-    public SMargin(int[] _data)
-    {
-        if (_data.Length != 4)
-        {
-            top = 0;
-            bottom = 0;
-            right = 0;
-            left = 0;
-        }
-        else
-        {
-            top = _data[0];
-            bottom = _data[1];
-            right = _data[2];
-            left = _data[3];
-        }
     }
 }
