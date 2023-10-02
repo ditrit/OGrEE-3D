a2fff924f7213eb77dbdb5978fd0ce54462cc874
Timothé Rios <trios.timothe@hotmail.fr>
2023-10-02 18:08:02 +0200

    409 clearance v2 (#422)
    * Update Unity to 2022.3.9 (lastest LTS) and TriLib to 2.2.0
    
    * Added collision detection
    
    * Changed collision detection + Doc
    
     - Deleted ReliableOnTriggerExit (unoptimised), replaced by TriggerExitOnDisable
     - Toggled on walls colliders
     - Fixed bugs
    
    * Merging master into #409 (#421)
    
    * 361 component rework (#413)
    
    * Create Device, Corridor and Site classes
    
    * Create Domain class
    
    * Rename OObject to Item
    
    * Rename more variables to fit with new Item class
    
    * Update Unity and Trilib (#414)
    
    * Update Unity to 2022.3.9 (lastest LTS) and TriLib to 2.2.0
    
    * Disable Use Physical Keys in InputManager
    
    * Update README.md
    
    * Update build date and version
    
    * Delete position adjustment in rooms with specific axisOrientation (#419)
    
    * Code review (#420)
    
    * Code opti, typo, doc* Simplify item color change
    
    * Update Visual Studio package version
    
    * Code opti after new .net version + clear usings
    
    * Fix Site class for sites; Add components count in DebugText.CountObjects()
    
    * Simplify Item.SetColor() & Corridor.SetColor()
    
    * CreateSite returns Site
    
    * Update gitignore, remove Layouts/ and .vscode/ from tracking
    
    ---------
    
    Co-authored-by: Cédric Vautrain <35805113+Cedrok@users.noreply.github.com>
    Co-authored-by: Cedrok <cvautrai@student.42.fr>
    
    * Finished merge + renamed clearance shader
    
    * Better error message + Vector6
    
    * Fixed merge typo
    
    * Readded axisttext
    
    * Toggled on separators collider
    
    For clearances' collisions
    
    * Formatting
    
    * Formatting
    
    ---------
    
    Co-authored-by: Cedrok <cvautrai@student.42.fr>
    Co-authored-by: Cédric Vautrain <35805113+Cedrok@users.noreply.github.com>

---
+++ Assets/Scripts/ClearanceCollisionHandler.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class ClearanceCollisionHandler : MonoBehaviour
+{
+    [SerializeField] private new Renderer renderer;
+    public Transform ownObject;
+    [SerializeField] private int collisionCount = 0;
+
+    /// <summary>
+    /// Check if the other collider should be reacted to. If it is, give it a <see cref="TriggerExitOnDisable"/><br/>
+    /// if it hasn't got one already and set it to notify us should the other gameobject be disabled.<br/>
+    /// Then add one to the collision count and set the clearance color to red.
+    /// </summary>
+    /// <param name="_other">the collider colliding with us</param>
+    private void OnTriggerEnter(Collider _other)
+    {
+        if (_other.transform.IsChildOf(ownObject) || _other.transform.parent?.GetComponent<Building>())
+            return;
+        if (!_other.TryGetComponent(out TriggerExitOnDisable trigger))
+            trigger = _other.gameObject.AddComponent<TriggerExitOnDisable>();
+        trigger.shouldFire = true;
+        trigger.collisionHandler = this;
+        collisionCount++;
+        renderer.material.color = Color.red;
+    }
+
+    /// <summary>
+    /// Check if the other collider should be reacted to. If it is, deactivate its <see cref="TriggerExitOnDisable"/>
+    /// and substract one to the collision count and set the clearance color to green if it reaches 0
+    /// </summary>
+    /// <param name="_other">the collider colliding with us</param>
+    private void OnTriggerExit(Collider _other)
+    {
+        if (_other.transform.IsChildOf(ownObject) || _other.transform.parent?.GetComponent<Building>())
+            return;
+        _other.GetComponent<TriggerExitOnDisable>().shouldFire = false;
+        SubstractCollision();
+    }
+
+    /// <summary>
+    /// Substract one to the collision count and set the clearance color to green if it reaches 0
+    /// </summary>
+    public void SubstractCollision()
+    {
+        collisionCount--;
+        if (collisionCount == 0)
+            renderer.material.color = Color.green;
+    }
+
+
+    /// <summary>
+    /// Reset the collision count and the clearane color on disabling
+    /// </summary>
+    private void OnDisable()
+    {
+        collisionCount = 0;
+        renderer.material.color = Color.green;
+    }
+}
