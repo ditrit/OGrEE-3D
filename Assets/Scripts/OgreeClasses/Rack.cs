88c4ad41e9db1b3e9236f6c569827f200d5dc780
Cedrok <cvautrai@student.42.fr>
2024-04-10 18:45:22 +0200

    Forgot Rack....

--- Assets/Scripts/OgreeClasses/Rack.cs
+++ Assets/Scripts/OgreeClasses/Rack.cs
@@ -18,10 +18,7 @@ public class Rack : Item
         }
 
         if (domain != _src.domain)
-        {
-            domain = _src.domain;
-            UpdateColorByDomain();
-        }
+            UpdateColorByDomain( _src.domain);
 
         if (HasAttributeChanged(_src, "color"))
             SetColor(_src.attributes["color"]);
